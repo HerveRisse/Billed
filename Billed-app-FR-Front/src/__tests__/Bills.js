80c1b4380661b0b8096093605133695064e7c10e
herve risse <herve.risse91@gmail.com>
2023-09-05 00:53:59 +0800

    test get bills.js

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,11 +2,13 @@
  * @jest-environment jsdom
 */
 
-import { screen, waitFor } from "@testing-library/dom"
+import { fireEvent, screen, waitFor } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import '@testing-library/jest-dom/extend-expect';
+
 
 import router from "../app/Router.js";
 
@@ -26,7 +28,7 @@ describe("Given I am connected as an employee", () => {
             await waitFor(() => screen.getByTestId('icon-window'))
             const windowIcon = screen.getByTestId('icon-window')
             //to-do write expect expression
-
+            expect(windowIcon).toHaveClass("active-icon")
         })
         test("Then bills should be ordered from earliest to latest", () => {
             document.body.innerHTML = BillsUI({ data: bills })
@@ -34,6 +36,51 @@ describe("Given I am connected as an employee", () => {
             const antiChrono = (a, b) => ((a < b) ? 1 : -1)
             const datesSorted = [...dates].sort(antiChrono)
             expect(dates).toEqual(datesSorted)
+        })
+    })
+    /* ----------------------------------------------------------------*/
+
+    // test d'intégration GET
+        describe("When I am on Bills Page", () => {
+        test("the bills should be fetched", async () => {
+            Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+            window.localStorage.setItem('user', JSON.stringify({
+                type: 'Employee'
+            }))
+            const root = document.createElement("div");
+            root.setAttribute("id", "root");
+            document.body.append(root);
+            router();
+            window.onNavigate(ROUTES_PATH.Bills);
+            const tableContainer = await waitFor(() => screen.getByTestId("tbody"));
+            expect(tableContainer).toBeTruthy();
+            const rows = screen.getAllByRole("row", { container: tableContainer });
+            expect(rows).toHaveLength(5);
+        });
+    });
+
+    describe("When I am on Bills Page", () => {
+        test("Then the clickable button should be present on the page", async () => {
+
+            Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+            window.localStorage.setItem('user', JSON.stringify({
+                type: 'Employee'
+            }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.append(root)
+            router()
+            window.onNavigate(ROUTES_PATH.Bills)
+
+            await waitFor(() => screen.getByTestId('btn-new-bill'))
+            const newBillBtn = screen.getByTestId('btn-new-bill')
+            expect(newBillBtn).toBeVisible();
+
+            await waitFor(() => screen.getAllByTestId('icon-eye'))
+            const eyeIcons = screen.getAllByTestId('icon-eye')
+            eyeIcons.forEach((eyeIcon) => {
+                expect(eyeIcon).toBeVisible();
+            });
         })
     })
 })
